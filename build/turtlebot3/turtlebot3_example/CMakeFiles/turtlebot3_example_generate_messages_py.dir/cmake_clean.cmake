38a9e59cbd8de45b474c629e36169b6a2ba875b2
Jacob Sayono <jrsayono@gmail.com>
2023-03-07 05:58:13 -0800

    done

---
+++ build/turtlebot3/turtlebot3_example/CMakeFiles/turtlebot3_example_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "/home/jacobsayono/wanderbot_ws/devel/lib/python3/dist-packages/turtlebot3_example/msg/_Turtlebot3Action.py"
+  "/home/jacobsayono/wanderbot_ws/devel/lib/python3/dist-packages/turtlebot3_example/msg/_Turtlebot3ActionFeedback.py"
+  "/home/jacobsayono/wanderbot_ws/devel/lib/python3/dist-packages/turtlebot3_example/msg/_Turtlebot3ActionGoal.py"
+  "/home/jacobsayono/wanderbot_ws/devel/lib/python3/dist-packages/turtlebot3_example/msg/_Turtlebot3ActionResult.py"
+  "/home/jacobsayono/wanderbot_ws/devel/lib/python3/dist-packages/turtlebot3_example/msg/_Turtlebot3Feedback.py"
+  "/home/jacobsayono/wanderbot_ws/devel/lib/python3/dist-packages/turtlebot3_example/msg/_Turtlebot3Goal.py"
+  "/home/jacobsayono/wanderbot_ws/devel/lib/python3/dist-packages/turtlebot3_example/msg/_Turtlebot3Result.py"
+  "/home/jacobsayono/wanderbot_ws/devel/lib/python3/dist-packages/turtlebot3_example/msg/__init__.py"
+  "CMakeFiles/turtlebot3_example_generate_messages_py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/turtlebot3_example_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/wanderbot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jacobsayono/wanderbot_ws/src/wanderbot
+# Build directory: /home/jacobsayono/wanderbot_ws/build/wanderbot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
